b1a324b2e6b16445eeb8d8853e635ce114851715
lurkersaqz <127319726+lurkersaqz@users.noreply.github.com>
2023-03-08 17:24:12 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lurkersaqz/kuwa/main/loadfunction.lua")()
-    end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lurkersaqz/kuwa/main/loadfunction.lua"))()
+        end)
